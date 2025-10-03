5163950e10ebd8811d814e1f364ab4b3da171271
Đỗ Cao Nghĩa <44649794+donghia9x@users.noreply.github.com>
2025-10-03 14:54:38 +0700

    Update layout.jsx
    modify to valid java script import

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,24 +9,33 @@ export const metadata = {
     }
 };
 
+const gtmScript = `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W9W2L7N5');
+`;
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
                 {/* Google Tag Manager */}
-                <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-W9W2L7N5');</script>
+                <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
                 {/* End Google Tag Manager */}
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
             <body className="antialiased text-white bg-blue-900">
                 {/* Google Tag Manager (noscript) */}
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9W2L7N5"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-                {/* End Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe 
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-W9W2L7N5"
+                        height="0" 
+                        width="0" 
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
 
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
