a14d0f964fd6ad51dcc30e101dd0e071b78a9680
Đỗ Cao Nghĩa <44649794+donghia9x@users.noreply.github.com>
2025-10-08 23:06:14 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -47,7 +47,7 @@ export function executeLoginGATag({ method, userId }) {
             window.dataLayer.push({
                 'event': 'login',
                 'method': method,
-                // user_id ĐÃ BỊ LOẠI BỎ khỏi event payload
+                'user_id': userId,
                 'test_custom_param': 'initialized'
             });
             // Đánh dấu rằng sự kiện đã được gửi
