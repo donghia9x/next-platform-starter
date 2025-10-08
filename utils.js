0a247f10236d77964def717927b6a10c520813f2
Đỗ Cao Nghĩa <44649794+donghia9x@users.noreply.github.com>
2025-10-08 22:57:27 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -113,9 +113,6 @@ export function getCurrentUser() {
 export function loginUser(user) {
     if (typeof window !== 'undefined') {
         sessionStorage.setItem(AUTH_KEY, JSON.stringify(user));
-
-        // Đặt giá trị User ID (cho Configuration Tag)
-        setUserIdInDatalayer(user.id);
         
         // Kích hoạt sự kiện GA ngay sau khi lưu session (chỉ một lần)
         executeLoginGATag({ method: user.method, userId: user.id });
